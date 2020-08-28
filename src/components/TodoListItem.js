e0bbf660b353fcd90c5885ed320fff6cdc11c4c9
Alexander Balabanov <baal72alex@gmail.com>
2020-08-28 15:52:43 +0300

    Fix handlers in item

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,23 +1,32 @@
 //Core
 import React from 'react';
 
-function TodoListItem({todo, index, onCopmplite,removeTodo}){
+function TodoListItem({todo, index, onComplite,removeTodo}){
   let clases=[]
   if(todo.done){
     clases.push('done')
   }
+
+  function handlerOnChangeСheckbox(){
+    onComplite(todo.id)
+  }
+
+  function handlerOnClickRemove(){
+    removeTodo(todo.id)
+  }
+
   return(
     <li className={'li_class'}>
       <span className={clases.join(' ')}>
         <input
           type='checkbox'
-          onChange={()=>onCopmplite(todo.id)}
+          onChange={handlerOnChangeСheckbox}
           checked={todo.done}
         />
         {index +1 }
         {todo.title}
       </span>
-      <button onClick={()=>removeTodo(todo.id)}>&times;</button>
+      <button onClick={handlerOnClickRemove}>&times;</button>
     </li>
   );
 }
